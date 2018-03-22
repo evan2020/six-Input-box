cb33eb79f0ed17e19c5994dc5511fc84a91f231a
hudashuang <hudashuang@q.com>
2018-03-22 17:50:09 +0800

    新增是否明文显示选项

--- component/component.js
+++ component/component.js
@@ -1,6 +1,6 @@
 // component.js
 Component({
-  
+
   // 组件属性
   properties: {
 
@@ -13,14 +13,19 @@ Component({
         let that = this;
         // 当输入框的值等于6时（发起支付等...）
         if (newVal == 6) {
-          // 引用组件页面的自定义函数
-          that.triggerEvent('valueSix', {}, {})
-          // 回到初始样式
-          that.setData({
-            get_focus: false,
-            value_length: 0,
-            input_value: ""
-          });
+
+          // 设定延时事件处理
+          setTimeout(function () {
+            // 引用组件页面的自定义函数
+            that.triggerEvent('valueSix', {}, {})
+            // 回到初始样式
+            that.setData({
+              get_focus: false,
+              value_length: 0,
+              input_value: ""
+            });
+          }, 500)
+
         }
       }
     },
@@ -76,7 +81,7 @@ Component({
 
   // 初始化数据
   data: {
-
+    see: true,//是否明文展示
   },
 
   // 组件方法
@@ -109,9 +114,20 @@ Component({
     // 获取输入框的值
     get_value(data) {
       let that = this;
+      // 设置空数组用于明文展现
+      let val_arr = [];
+      // 获取当前输入框的值
+      let now_val = data.detail.value
+      // 遍历把每个数字加入数组
+      for (let i = 0; i < 6; i++) {
+        val_arr.push(now_val.substr(i, 1))
+      }
+      // 获取输入框值的长度
       let value_length = data.detail.value.length;
+      // 更新数据
       that.setData({
-        value_length: value_length
+        value_length: value_length,
+        val_arr: val_arr
       });
 
     },
--- index/index.js
+++ index/index.js
@@ -4,21 +4,21 @@ const app = getApp()
 
 Page({
   data: {
-    
+
   },
 
   // 当组件输入数字6位数时的自定义函数
-  valueSix(){
+  valueSix() {
     console.log("1");
     // 模态交互效果
     wx.showToast({
-      title: '第一个组件',
+      title: '支付成功',
       icon: 'success',
       duration: 2000
     })
   },
 
-  valueFail(){
+  valueFail() {
     console.log("2");
     // 模态交互效果
     wx.showToast({
@@ -27,5 +27,18 @@ Page({
       duration: 2000
     })
   },
+  onload: function () {
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+      }
+    })
+  }
 
 })
